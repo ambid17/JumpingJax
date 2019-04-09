25ad6c371c8a796fc92c63fbaa5cde807eb3b42d
ambid17 <sambrose@nebrwesleyan.edu>
2019-04-08 22:51:12 -0500

    Added inital implementation
    slight variant of: https://github.com/WiggleWizard/quake3-movement-unity3d

---
+++ BunnyHopMaster/Assets/PlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+    Camera mainCamera;
+    public Vector3 headPositionOffset;
+    // Start is called before the first frame update
+    void Start() {
+        headPositionOffset = new Vector3(0, 1.7f, 0);
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        mainCamera.transform.position = transform.position + headPositionOffset;
+        mainCamera.transform.rotation = transform.rotation;
+    }
+}
