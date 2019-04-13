02ff10f525c3d0f9297562c7a2c6dc7f76b9d007
ambid17 <12086352+ambid17@users.noreply.github.com>
2019-04-13 03:22:12 -0500

    Revert "Revert "Added checkpoints""
    This reverts commit 65ba7a546d5b5887061eeb2bd739eca532ada754.

---
+++ BunnyHopMaster/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Checkpoint currentCheckpoint;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
