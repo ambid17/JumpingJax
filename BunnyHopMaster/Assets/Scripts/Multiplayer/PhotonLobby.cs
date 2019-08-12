61cde21b906c45d85724625d42d142ccce987900
ambid17 <12086352+ambid17@users.noreply.github.com>
2019-08-11 19:45:36 -0500

    Added photon and debug window

---
+++ BunnyHopMaster/Assets/Scripts/Multiplayer/PhotonLobby.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public GlobalDebug debugger;
+
+    public static PhotonLobby lobby;
+
+    public GameObject joinButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        debugger = GetComponent<GlobalDebug>();
+        lobby = this; //creates singleton in lobby scene
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        debugger.Log("Player connected to master server");
+        joinButton.SetActive(true);
+
+    }
+    
+
+    public void OnJoinButtonClicked()
+    {
+        joinButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        debugger.Log("Couldn't join random room, there must not be any available, creating one now");
+        CreateRoom();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        debugger.Log("Couldn't create room, must be one with same name, making a new one");
+        CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        debugger.Log("We have joined a room");
+    }
+
+    void CreateRoom()
+    {
+        debugger.Log("trying to create a new room");
+        int randomRoomName = Random.Range(0, 1000);
+        RoomOptions roomOptions = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOptions);
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        cancelButton.SetActive(false);
+        joinButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
---
+++ BunnyHopMaster/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 newRotation = gameObject.transform.rotation.eulerAngles + new Vector3(0, Time.deltaTime, 0);
+        Quaternion q = Quaternion.Euler(newRotation.x, newRotation.y, newRotation.z);
+        gameObject.transform.rotation.Set(q.x, q.y, q.z, q.w);
+    }
+}
