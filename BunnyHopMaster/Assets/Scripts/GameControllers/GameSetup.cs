20fbfd3e15a3049fecb37b9491eea6bdfa00c3ab
ambid17 <12086352+ambid17@users.noreply.github.com>
2019-08-12 01:21:38 -0500

    WIP adding disconnection
    need to add some script that persists across scenes for disconnection to work properly

--- BunnyHopMaster/Assets/Scripts/GameControllers/GameSetup.cs
+++ BunnyHopMaster/Assets/Scripts/GameControllers/GameSetup.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameSetup : MonoBehaviour
@@ -28,5 +29,25 @@ public class GameSetup : MonoBehaviour
     {
         Debug.Log("Creating Player");
         PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+    }
+
+    public void DisconnectPlayer()
+    {
+        StartCoroutine(DisconnectAndLoad());
+    }
+
+    IEnumerator DisconnectAndLoad()
+    {
+        //TODO: make sure to sync all variables and make sure everything correct is destroyed 
+        //SO, once object picking up is done, make sure the object isn't deleted when a player disconnects
+        //Use PhotonNetwork.InstantiateSceneObject() instead of .Instantiate()
+        PhotonNetwork.LeaveRoom();
+        while (PhotonNetwork.InRoom)
+        {
+            yield return null;
+        }
+
+        //TODO use MultiplayerSettings.menuScene
+        SceneManager.LoadScene(0);
     }
 }
