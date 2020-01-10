504533c8928b2ff8265cbaa83bbb041aeead928e
Omaranwa <omar.aglan91@yahoo.com>
2020-01-10 21:58:01 +0200

    Update the Credits Menu
    1- Quit Button
    2- Main Menu Button

---
+++ BunnyHopMaster/Assets/Scripts/UI/ShortMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShortMenu : MonoBehaviour {
+	
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
