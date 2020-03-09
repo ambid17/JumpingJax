206fec7f7635c291d4213af20139717d112c53ea
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-03-09 18:19:20 -0500

    removed old builds

--- BunnyHopMaster/Assets/Scripts/Portal/PortalPlacement.cs
+++ BunnyHopMaster/Assets/Scripts/Portal/PortalPlacement.cs
@@ -11,11 +11,14 @@ public class PortalPlacement : MonoBehaviour
     [SerializeField]
     private LayerMask layerMask;
 
+    // Leaving this in until crosshair is added
     //[SerializeField]
     //private Crosshair crosshair;
 
     private CameraMove cameraMove;
 
+    private Quaternion flippedYRotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+    private const float portalRaycastDistance = 250;
     private void Awake()
     {
         cameraMove = GetComponent<CameraMove>();
@@ -58,12 +61,12 @@ public class PortalPlacement : MonoBehaviour
 
                 // Update position of raycast origin with small offset.
                 Vector3 relativePos = inPortal.transform.InverseTransformPoint(hit.point + dir);
-                relativePos = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativePos;
+                relativePos = flippedYRotation * relativePos;
                 pos = outPortal.transform.TransformPoint(relativePos);
 
                 // Update direction of raycast.
                 Vector3 relativeDir = inPortal.transform.InverseTransformDirection(dir);
-                relativeDir = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativeDir;
+                relativeDir = flippedYRotation * relativeDir;
                 dir = outPortal.transform.TransformDirection(relativeDir);
 
                 distance -= Vector3.Distance(pos, hit.point);
