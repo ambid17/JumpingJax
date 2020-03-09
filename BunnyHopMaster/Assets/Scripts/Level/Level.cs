1ec2637af4c80e7ee30f620a987d416ac63c70c2
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-03-09 17:53:58 -0500

    code cleanup

--- BunnyHopMaster/Assets/Scripts/Level/Level.cs
+++ BunnyHopMaster/Assets/Scripts/Level/Level.cs
@@ -8,24 +8,8 @@ public class Level
     public int levelBuildIndex;
     [SerializeField]
     public string levelName;
-
     [SerializeField]
     public bool isCompleted;
     [SerializeField]
     public float completionTime;
-
-    //public Level(int levelNumber, float completionTime, string levelName)
-    //{
-    //    this.levelBuildIndex = levelNumber;
-    //    this.levelName = levelName;
-    //    if(completionTime == float.PositiveInfinity)
-    //    {
-    //        this.isCompleted = false;
-    //    }
-    //    else
-    //    {
-    //        this.isCompleted = true;
-    //    }
-    //    this.completionTime = completionTime;
-    //}
 }
