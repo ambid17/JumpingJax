2f14d6df0663dd06d92efd2b74ca5282b1a5747d
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-03-10 19:04:18 -0500

    Fixed bug due to player prefab requiring portals

--- BunnyHopMaster/Assets/Scripts/Level/Level.cs
+++ BunnyHopMaster/Assets/Scripts/Level/Level.cs
@@ -4,12 +4,17 @@ using System;
 [Serializable]
 public class Level
 {
+    [Header("Set in Editor")]
     [SerializeField]
     public int levelBuildIndex;
     [SerializeField]
     public string levelName;
     [SerializeField]
+    public bool isPortalLevel;
+    [Header("Set in Game")]
+    [SerializeField]
     public bool isCompleted;
     [SerializeField]
     public float completionTime;
+    
 }
