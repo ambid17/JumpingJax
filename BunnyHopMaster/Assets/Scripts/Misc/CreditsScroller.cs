53a669beefb48622965ec148551ed52eff4ce3ec
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-03-14 19:47:51 -0500

    added credits scene

---
+++ BunnyHopMaster/Assets/Scripts/Misc/CreditsScroller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsScroller : MonoBehaviour
+{
+    public GameObject textContainer;
+    public Button exitButton;
+    public float endingY = 0;
+    public float scrollSpeed = 0.5f;
+
+    void Start()
+    {
+        exitButton.onClick.AddListener(GoToMainMenu);
+        exitButton.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(textContainer.transform.localPosition.y < endingY)
+        {
+            textContainer.transform.position += new Vector3(0, scrollSpeed, 0);
+        }
+        else
+        {
+            exitButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        Cursor.visible = true;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
