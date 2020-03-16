60c92a9d5855a82b4431baca328b3c8223bcf044
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-03-16 12:29:59 -0500

    updated bhop 9

---
+++ BunnyHopMaster/Assets/Scripts/Level/UntouchableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UntouchableObject : MonoBehaviour
+{
+    private void Start()
+    {
+        Collider myCollider = GetComponent<Collider>();
+        myCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerProgress playerProgress = other.GetComponent<PlayerProgress>();
+        playerProgress.Respawn();
+    }
+}
