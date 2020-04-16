83b0101fed901a3266c2d2d3f3df8d0c539b0c11
fuhmac <fuhrmann.maciej@gmail.com>
2020-04-16 18:50:31 +0200

    Added some dependencies for nsubstitute

---
+++ BunnyHopMaster/Assets/Tests/CheckpointsCountTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CheckpointCountTest
+    {
+        [Test]
+        public void TestSuiteSimplePasses()
+        {
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestSuiteWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
