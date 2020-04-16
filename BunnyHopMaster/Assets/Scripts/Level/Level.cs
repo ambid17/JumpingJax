f1d3f30d6fd33ffd387c0cd4c6033200c3ff3292
fuhmac <fuhrmann.maciej@gmail.com>
2020-04-16 17:36:00 +0200

    Fixed reseting level values on build and play

--- BunnyHopMaster/Assets/Scripts/Level/Level.cs
+++ BunnyHopMaster/Assets/Scripts/Level/Level.cs
@@ -7,19 +7,32 @@ public class Level : ScriptableObject
     [Header("Set in Editor")]
     [SerializeField]
     public int levelBuildIndex;
+
     [SerializeField]
     public string levelName;
+
     [SerializeField]
     public bool isPortalLevel;
+
     [SerializeField]
     public bool isSurfLevel;
+
     [SerializeField]
     public string[] tutorialTexts;
+
     [SerializeField]
     public int numberOfCheckpoints;
+
     [Header("Set in Game")]
     [SerializeField]
     public bool isCompleted;
+
     [SerializeField]
     public float completionTime;
+
+    private void OnEnable()
+    {
+        isCompleted = false;
+        completionTime = 0f;
+    }
 }
