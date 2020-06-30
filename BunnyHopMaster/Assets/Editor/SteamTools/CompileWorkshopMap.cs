fbfe8a942b547f1a8da7858bc78dbe5970576767
Sabien <ambid17@gmail.com>
2020-06-30 12:45:10 -0500

    WIP started creating workshop exporter

---
+++ BunnyHopMaster/Assets/Editor/SteamTools/CompileWorkshopMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.Experimental.AssetBundlePatching;
+using UnityEditor.VersionControl;
+
+public class CompileWorkshopMap : EditorWindow
+{
+    [MenuItem("Tools/Compile Map")]
+    private static void CompileMap()
+    {
+        string assetBundleDirectory = Application.streamingAssetsPath + "/Maps";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+                                        BuildAssetBundleOptions.None,
+                                        BuildTarget.StandaloneWindows);
+    }
+}
