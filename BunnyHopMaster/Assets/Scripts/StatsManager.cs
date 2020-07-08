89a8b6d9c9785f9e6889f15f063f88349a350881
Sabien <ambid17@gmail.com>
2020-07-08 18:04:21 -0500

    Added working steam level stats update

---
+++ BunnyHopMaster/Assets/Scripts/StatsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class StatsManager : MonoBehaviour
+{
+    public static void TryUpdateStat(string levelName, float newCompletionTime)
+    {
+        var levelCompletionTime = Steamworks.SteamUserStats.GetStatFloat(levelName);
+
+        if(newCompletionTime < levelCompletionTime || levelCompletionTime == 0)
+        {
+            Steamworks.SteamUserStats.SetStat(levelName, newCompletionTime);
+        }
+
+        Steamworks.SteamUserStats.StoreStats();
+    }
+}
